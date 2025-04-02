86350ba167b9243a440063f1700b1caaaa2a6e7f
jogofer <jonathangomezfdez@gmail.com>
2025-04-02 12:10:49 +0200

    added constants file for searchInput

--- src/components/QuickSearch/SearchInput/SearchInput.tsx
+++ src/components/QuickSearch/SearchInput/SearchInput.tsx
@@ -1,12 +1,11 @@
 import { SearchInputStyled } from './SearchInput.styled';
+import { DEFAULT_PLACEHOLDER } from "./SearchInput.constants"
 
 interface SearchInputProps {
   query: string;
   setQuery: (value: string) => void;
   placeholder?: string;
 }
-
-const DEFAULT_PLACEHOLDER = "Search...";
 
 export const SearchInput = ({ query, setQuery, placeholder = DEFAULT_PLACEHOLDER }: SearchInputProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value);
